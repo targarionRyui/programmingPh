6134c9c240aa6edd664e348c7a9803abf586b652
targarionRyui <kinjagodevs@gmail.com>
2025-12-27 09:11:34 +0900

    Add files via upload
    Олимп

---
+++ 8dayZad1.py
@@ -0,0 +1,65 @@
+
+# Задание 1
+# p = input()
+# p = list(p)
+# p = sorted(p)
+# sor = sorted(p)
+# sord = []
+# ind = []
+# for i in range(len(p)):
+#     c = 0
+#     for j in range(len(p)):
+#         if sor[i] == sor[j]:
+#             c += 1
+#             if c > 1:
+#                 sor[i] = -1
+# kol = 0
+# for i in sor:
+#     if int(i) > -1:
+#       kol += 1
+#       sord.append(i)
+# print(p)
+# print(sor)
+# print(kol)
+# print(*sord)
+
+
+# Задание 1.1 не докончено
+# dup = []
+# org = []
+#
+#
+# def f(n):
+#     if n[0] >= n[1]:
+#         return f(n[1:])
+#     if:
+#         del n[1]
+#         return f(n)
+#     return n
+# print(f(list('6254256')))
+
+
+# Задание 2
+# sord = []
+# skilk = int(input())
+# x = []
+# for i in range(skilk):
+#     x.append(int(input()))
+# x = sorted(x)
+# for i in range(len(x)):
+#     c = 0
+#     for j in range(len(x)):
+#         if x[i] == x[j]:
+#             c += 1
+#             if c > 1:
+#                 x[i] = -1
+# kol = 0
+# for i in x:
+#     if int(i) > -1:
+#       kol += 1
+#       sord.append(i)
+#
+# print(sord)
+# print(kol)
+
+# Задание 3
